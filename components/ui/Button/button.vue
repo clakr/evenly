b0fd26e33e9fae976ca478d45915f382a3f1750b
clakr <clarktolosa@gmail.com>
2024-10-08 19:30:21 +0800

    refactored login ui

--- components/ui/Button/button.vue
+++ components/ui/Button/button.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
+import type { NuxtLinkProps } from "#app";
 import { twMerge } from "tailwind-merge";
 import { computed, type ButtonHTMLAttributes } from "vue";
 
-type Variant = "filled";
+type Variant = "filled" | "link";
 
 const props = withDefaults(
-  defineProps<{ class?: ButtonHTMLAttributes["class"]; variant?: Variant }>(),
+  defineProps<{
+    class?: ButtonHTMLAttributes["class"];
+    variant?: Variant;
+    to?: NuxtLinkProps["to"];
+  }>(),
   {
     class: "",
     variant: "filled",
+    to: "",
   },
 );
+
+const DEFAULT_CLASSES =
+  "whitespace-nowrap rounded text-sm font-medium focus-within:outline focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-slate-600";
 
 const variants = new Map<Variant, string>([
   [
     "filled",
-    "whitespace-nowrap rounded bg-slate-600 px-4 py-2 text-sm font-medium text-white focus-within:outline focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-slate-600 [&:is(:hover,:focus)]:bg-slate-700",
+    "bg-slate-600 px-4 py-2 text-white [&:is(:hover,:focus)]:bg-slate-700",
   ],
+  ["link", "text-slate-600 hover:underline"],
 ]);
 
 const classes = computed(() =>
-  twMerge("", variants.get(props.variant), props.class),
+  twMerge(DEFAULT_CLASSES, variants.get(props.variant), props.class),
 );
 </script>
 
 <template>
-  <button :class="classes">
+  <NuxtLink v-if="to" :to :class="classes">
+    <slot />
+  </NuxtLink>
+  <button v-else :class="classes">
     <slot />
   </button>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -32,47 +32,52 @@ async function handleLoginUser() {
 
 <template>
   <main class="grid min-h-screen place-content-center">
-    <form
-      class="flex w-[calc(100svw-2rem)] max-w-sm flex-col gap-y-3 rounded border border-slate-200 bg-white p-4 shadow shadow-slate-200"
-      @submit.prevent="handleLoginUser"
+    <div
+      class="flex w-[calc(100svw-2rem)] max-w-sm flex-col gap-y-4 rounded border border-slate-200 bg-white p-4 shadow-sm shadow-slate-200"
     >
-      <header class="flex flex-col gap-y-1">
+      <header>
         <h1 class="text-2xl font-bold">Login</h1>
         <p class="text-sm text-slate-950/50">
           Enter your email address and password to login to your account.
         </p>
       </header>
-      <div class="flex flex-col gap-y-1">
-        <Label for="email">Email Address</Label>
-        <Input
-          id="email"
-          v-model="form.email"
-          type="email"
-          name="email"
-          autocomplete="email"
-          required
-        />
-        <FormErrors
-          v-if="errors.nested?.email"
-          :errors="errors.nested?.email"
-        />
-      </div>
-      <div class="flex flex-col gap-y-1">
-        <Label for="password">Password</Label>
-        <Input
-          id="password"
-          v-model="form.password"
-          type="password"
-          name="password"
-          autocomplete="current-password"
-          required
-        />
-        <FormErrors
-          v-if="errors.nested?.password"
-          :errors="errors.nested?.password"
-        />
-      </div>
-      <Button class="mt-3">Sign in</Button>
-    </form>
+      <form class="flex flex-col gap-y-2" @submit.prevent="handleLoginUser">
+        <div class="flex flex-col gap-y-1">
+          <Label for="email">Email Address</Label>
+          <Input
+            id="email"
+            v-model="form.email"
+            type="email"
+            name="email"
+            autocomplete="email"
+            required
+          />
+          <FormErrors
+            v-if="errors.nested?.email"
+            :errors="errors.nested?.email"
+          />
+        </div>
+        <div class="flex flex-col gap-y-1">
+          <Label for="password">Password</Label>
+          <Input
+            id="password"
+            v-model="form.password"
+            type="password"
+            name="password"
+            autocomplete="current-password"
+            required
+          />
+          <FormErrors
+            v-if="errors.nested?.password"
+            :errors="errors.nested?.password"
+          />
+        </div>
+        <Button class="mt-2">Sign in</Button>
+      </form>
+      <footer class="text-center text-sm">
+        Already have an account?
+        <Button variant="link" to="/register">Sign up</Button>
+      </footer>
+    </div>
   </main>
 </template>
